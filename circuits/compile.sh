e9fe68923522da0d290e29d75fd7ff447da56e9b
Joe Polny <joepolny@gmail.com>
2026-04-12 18:00:41 -0400

    transfer circuit

--- circuits/compile.sh
+++ circuits/compile.sh
@@ -9,3 +9,7 @@ pnpm snarkjs plonk setup $CIRCUITS_DIR/out/claim.r1cs $CIRCUITS_DIR/setup/pot16_
 circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/deposit.circom --prime bls12381 -o out
 pnpm snarkjs plonk setup $CIRCUITS_DIR/out/deposit.r1cs $CIRCUITS_DIR/setup/pot16_final.ptau $CIRCUITS_DIR/zkeys/deposit.zkey
 
+circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/transfer.circom --prime bls12381 -o out
+pnpm snarkjs plonk setup $CIRCUITS_DIR/out/transfer.r1cs $CIRCUITS_DIR/setup/pot16_final.ptau $CIRCUITS_DIR/zkeys/transfer.zkey
+
+
