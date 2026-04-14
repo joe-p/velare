885a051ba41260a67719ea09d6523f6846c3ca8c
Joe Polny <joepolny@gmail.com>
2026-04-14 14:52:46 -0400

    mkdir circuits/circom

--- circuits/circom/spend.circom
+++ circuits/circom/spend.circom
@@ -1,8 +1,8 @@
 pragma circom 2.1.5;
 
 include "./utxo.circom";
-include "./node_modules/circomlib/circuits/bitify.circom";
-include "./node_modules/circomlib/circuits/comparators.circom";
+include "../node_modules/circomlib/circuits/bitify.circom";
+include "../node_modules/circomlib/circuits/comparators.circom";
 
 template SumU64(N) {
     signal input amounts[N];
--- circuits/compile.sh
+++ circuits/compile.sh
@@ -5,7 +5,7 @@ CIRCUITS_DIR=$(pwd)
 
 bash ./setup/setup.sh
 
-circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/spend.circom --prime bls12381 -o out
+circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/circom/spend.circom --prime bls12381 -o out
 pnpm snarkjs plonk setup $CIRCUITS_DIR/out/spend.r1cs $CIRCUITS_DIR/setup/pot16_final.ptau $CIRCUITS_DIR/zkeys/spend.zkey
 
 
